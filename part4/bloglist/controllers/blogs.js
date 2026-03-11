fc5b177752ef8fa3e9b0b9989ee09b334af92f21
Muhammad Ain Uddin <muhammadainx@gmail.com>
2026-03-11 15:54:18 +0500

    feat(part4): complete bloglist tests exercises 4.8-4.12

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,13 +2,12 @@ const blogsRouter = require("express").Router();
 
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const { title, author, url, likes = 0 } = request.body;
 
   if (!title || !author || !url) {
@@ -19,9 +18,8 @@ blogsRouter.post("/", (request, response) => {
 
   const blog = new Blog({ title, author, url, likes });
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,125 @@
+const { describe, test, beforeEach, after } = require("node:test");
+const assert = require("node:assert");
+
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+
+const app = require("../app");
+const helper = require("./test_helper");
+
+const Blog = require("../models/blog");
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+describe("retrieval of all blogs", () => {
+  test("should return blogs as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("should return correct amount of blogs", async () => {
+    const response = await api.get("/api/blogs");
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  });
+
+  test("should have a unique identifier property named id", async () => {
+    const response = await api.get("/api/blogs");
+    response.body.forEach((blog) => {
+      assert.ok(blog.id);
+      assert.strictEqual(blog._id, undefined);
+    });
+  });
+});
+
+describe("addition of a new blog", () => {
+  test("should succeed with valid data", async () => {
+    const newBlog = {
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 3,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((b) => b.title);
+    assert(titles.includes(newBlog.title));
+  });
+
+  test("should default likes to 0 when property is missing", async () => {
+    const newBlog = {
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+    };
+
+    const response = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    assert.strictEqual(response.body.likes, 0);
+  });
+
+  test("should return 400 if title is missing", async () => {
+    const newBlog = {
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 3,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+
+  test("should return 400 if author is missing", async () => {
+    const newBlog = {
+      title: "React patterns",
+      url: "https://reactpatterns.com/",
+      likes: 3,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+
+  test("should return 400 if url is missing", async () => {
+    const newBlog = {
+      title: "React patterns",
+      author: "Michael Chan",
+      likes: 3,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
