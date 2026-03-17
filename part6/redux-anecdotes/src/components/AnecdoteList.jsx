f0782d7eafa92a382c07914bef05e70f46be4d9b
Muhammad Ain Uddin <muhammadainx@gmail.com>
2026-03-17 16:25:49 +0500

    feat(part6): complete anecdotes exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -4,7 +4,13 @@ import { voteAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
   const dispatch = useDispatch();
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    if (!filter) return anecdotes;
+
+    return anecdotes.filter((a) =>
+      a.content.toLowerCase().includes(filter.toLowerCase()),
+    );
+  });
 
   const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
 
